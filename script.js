d50a0e4a9686919b0fc1bfea51bafbdcca49d786
K-TrialBalloon <srinkar@hotmail.com>
2026-09-03 12:46:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -315,12 +315,12 @@ function getCurrentActivities(now) {
             
             speakAudio = false;
             /* We want to provide audio feedback only when the message display is active and only for messages
-               with an audioline that are within 5 to 3 mins of the actual entry sart time */
+               with an audioline that are within 8 to 3 mins of the actual entry sart time */
             
             if (AUDIO === 'ON' && 
                 showClock !== true && 
                 Object.hasOwn(activity, 'audioline') && 
-                difference <= 5 && 
+                difference <= 8 && 
                 difference >= 3) {
                 speakAudio = true;
                 }
